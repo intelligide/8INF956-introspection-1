8b812d2944ebb0172a1408a8b5b1937c0eff5735
yoann.potinet <yoann.potinet@gmail.com>
2020-02-27 01:32:02 -0500

    add src

---
+++ src/json/MyJSON.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace json
+{
+    public static class MyJSON
+    {
+        public static Dictionary<string, object> Serialize(object obj)
+        {
+            return SerializeObject(obj);
+        }
+
+        private static String GetBackingFieldName(String propertyName) => $"<{propertyName}>k__BackingField";
+
+        private static Dictionary<string, object> SerializeObject(object obj)
+        {
+            Dictionary<string, object> serialized = new Dictionary<string, object>();
+
+            foreach (FieldInfo fi in obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                Type ft = fi.FieldType;
+                object value = fi.GetValue(obj);
+                serialized.Add(fi.Name, SerializeValue(ft, value));
+            }
+
+            foreach (PropertyInfo fi in obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                var backingFieldName = GetBackingFieldName(fi.Name);
+                if(serialized.ContainsKey(backingFieldName))
+                {
+                    serialized.Remove(backingFieldName);
+                }
+
+                Type ft = fi.PropertyType;
+                object value = fi.GetValue(obj);
+                serialized.Add(fi.Name, SerializeValue(ft, value));
+            }
+
+            return serialized;
+        }
+
+        private static object SerializeValue(Type vt, object value)
+        {
+            if(value == null)
+            {
+                return null;
+            }
+            else if(vt == typeof(bool) || vt == typeof(char) || vt == typeof(decimal)  || 
+                vt == typeof(float) || vt == typeof(double) || vt == typeof(int) ||
+                vt == typeof(uint) || vt == typeof(long) || vt == typeof(ulong) || 
+                vt == typeof(short) || vt == typeof(ushort) || vt == typeof(string))
+            {
+                return value;
+            }
+            else if(vt.IsArray)
+            {
+                List<object> nested = new List<object>();
+                foreach(object item in (Array)value) 
+                {
+                    nested.Add(SerializeValue(item.GetType(), item));
+                }
+                return nested.ToArray();
+            }
+            else if(vt.GetInterfaces().Any(t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+            {
+                List<object> nested = new List<object>();
+                foreach(object item in (IEnumerable)value) 
+                {
+                    nested.Add(SerializeValue(item.GetType(), item));
+                }
+                return nested.ToArray();
+            }
+            else if(vt.IsClass)
+            {
+                return SerializeObject(value);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/main/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace main
+{
+    class Program
+    {
+        class OtherTestClass
+        {
+            int[] array1 = new int[] { 1, 3, 5, 7, 9 };
+
+            List<string> list1 = new List<string>()
+            {
+                "carrot",
+                "fox",
+                "explorer"
+            };
+        }
+
+        class TestClass
+        {
+            public bool field = true;
+
+            private OtherTestClass f = new OtherTestClass();
+
+            int intprop { get; set; } = 10;
+        }
+
+        static void Main(string[] args)
+        {
+            TestClass a = new TestClass();
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            Console.WriteLine(JsonSerializer.Serialize(json.MyJSON.Serialize(a), options));
+        }
+    }
+}
